0066ce5a8ba8b181c2bc9b6e7296c8ac96b14aa1
Jhon <jhonb3r@gmail.com>
2018-09-24 14:45:28 -0500

    Typo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.send('sorry, this is ony a backend')
+  res.send('sorry, this is only a backend')
 })
 
 module.exports = router
